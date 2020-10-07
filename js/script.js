71f7c8bf2fe828a5eba6d6076ab7dea65570c734
Alehander <sashok.rak@gmail.com>
2020-10-07 22:41:53 +0300

    07 10 push

--- js/script.js
+++ js/script.js
@@ -1,42 +1,60 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
-    count: "",
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 };
-let answer1 = prompt('Один из последних просмотренных фильмов?', ''),
-    answer2 = prompt('На сколько оцените его?', ''),
-    answer3 = prompt('Один из последних просмотренных фильмов?', ''),
-    answer4 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[answer1] = answer2;
-personalMovieDB.movies[answer3] = answer4;
+// for (let i = 1; i <= 2; i++) {
+// 	var answer1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 		answer2 = prompt('На сколько оцените его?', '');
 
+// 	if (answer1 != "" && answer2 != "" && answer1 != null && answer2 != null && answer1.length < 5 && answer1 != answer2) {
+// 		personalMovieDB.movies[answer1] = answer2;
+// 	} else {
+// 		i--;
+// 	}
+// }
+
+// var i = 1;
+// do {
+// 	var answer1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 		answer2 = prompt('На сколько оцените его?', '');
+// 	if (answer1 != "" && answer2 != "" && answer1 != null && answer2 != null && answer1.length < 5 && answer1 != answer2) {
+// 		personalMovieDB.movies[answer1] = answer2;
+// 	} else {
+// 		i--;
+// 	}
+// 	i++;
+// } while (i <= 2);
+
+// var i = 1;
+// while (i <= 2) {
+// 	var answer1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 		answer2 = prompt('На сколько оцените его?', '');
+// 	if (answer1 != "" && answer2 != "" && answer1 != null && answer2 != null && answer1.length < 5 && answer1 != answer2) {
+// 		personalMovieDB.movies[answer1] = answer2;
+// 	} else {
+// 		i--;
+// 	}
+// 	i++;	
+// }
+
+if (personalMovieDB.count != null && personalMovieDB.count != 0) {
+	if (personalMovieDB.count < 10) {
+		console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("Вы классический зритель");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("Вы киноман");
+	} else {
+		console.log("Произошла ошибка");
+	}
+} else {
+	console.log("Произошла ошибка");
+}
 console.log(personalMovieDB);
