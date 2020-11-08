1d5234934cc547b6714c1855ad16e95e89cf8d54
Alehander <sashok.rak@gmail.com>
2020-11-08 14:18:13 +0200

    1

--- js/function.js
+++ js/function.js
@@ -1,21 +1,13 @@
 "use strict";
 /* jshint esversion: 6 */
+// 1
+// function showfirstMess() {
+//    console.log("Hello");
+// }
+// showfirstMess();
 
-function calc(a, b) {
+function showfirstMess(a,b) {
    return a + b;
 }
-console.log(calc(5,5));
 
-let calcc = (a, b) => a+b;
-console.log(calcc(5,5));
-
-let str = "test";
-console.log(str.length);
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-let twelve = "12.4px";
-// console.log(Math.round(twelve));
-
-console.log(parseInt(twelve));
-console.log(parseFloat(twelve));
+console.log(showfirstMess(5,5));
